93dd454616ad60cb2ff15da422f1eb34ea075193
Meirinf <meirin.f@gmail.com>
2017-04-18 17:28:55 +0200

    El orco ya se mueve,salta y la camara lo persigue

--- Assets/Orc Warrior/Scripts/MouseController.cs
+++ Assets/Orc Warrior/Scripts/MouseController.cs
@@ -1,63 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseController : MonoBehaviour
-{
+public class MouseController : MonoBehaviour  {
+  public float jumpForce = 15.0f;
+  private Rigidbody2D rigidBody;
+  public float runningSpeed =5f;
+  public Animator animator;
+  public LayerMask groundLayerMas;
 
-    public LayerMask ground;
-    private Vector3 targetPosition;
-    public float speed=5;
-    public bool lookRight = true;
-    
-    private Animator animator;
 
-	void Start ()
-	{
-	    targetPosition = transform.position;
-	    animator = GetComponent<Animator>();
+  void Awake(){
+    rigidBody = GetComponent<Rigidbody2D>();
+  }
+	void Start (){
 	}
-	
 
+//Carga por cada Frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 8888, ground))
-            {
-                targetPosition = hit.point + new Vector3(0,0,-2);
-                //targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
-            }
-        }
-
-        if(Input.GetMouseButtonDown(1))
-        {
-            if(Random.Range(0f, 1.0f) > 0.5f)
-                animator.SetTrigger("attack");
-            else
-                animator.SetTrigger("special");
-        }
-
-       if (targetPosition.x > transform.position.x && !lookRight)
-          Flip();
-        if(targetPosition.x < transform.position.x && lookRight)
-            Flip();
-
-	    var p = transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * 	Time.deltaTime);
-
-	    //Vector3 vel = targetPosition - transform.position;
-        //vel = Vector3.ClampMagnitude(vel, speed * Time.deltaTime);
-        //transform.position += vel;
-
-        animator.SetFloat("speed", (transform.position - p).magnitude/Time.deltaTime);
-	}
-
-    public void Flip()
-    {
-        var s = transform.localScale;
-        s.x *= -1;
-        transform.localScale = s;
-        lookRight = !lookRight;
+    if(Input.GetMouseButtonDown(0)){
+      Jump();
     }
-}
+  }
+//Tal como esta programado mantiene siempre la misma velocidad y nunca para
+  void FixedUpdate(){
+    if(rigidBody.velocity.x < runningSpeed){
+      rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+    }
+  }
+
+//Clase para Saltar
+  void Jump(){
+      rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+      }
+
+      /*bool IsOnTheFloor(){
+        if (Physics2D.Raycast(this.transform.position, Vector2)){
+
+        }
+      }*/
+
+  }
